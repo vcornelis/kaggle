5650ce4d0df09709206d8e34a94127323e5db161
vcornelis <cornelis.vletter@newcraftgroup.com>
2016-01-11 20:59:04 +0100

    Update airbnb.R

--- airbnb.R
+++ airbnb.R
@@ -1,6 +1,9 @@
-library(plyr)
+#install.packages("e1071")
+#install.packages("rpart")
 
-#Project: https://www.kaggle.com/c/airbnb-recruiting-new-user-bookings
+library(plyr)
+library(e1071)
+library(rpart)
 
 #Set directory
 getwd()
@@ -15,14 +18,19 @@ train_users <- read.csv("~/Desktop/progs/kaggle/airbnb/train_users_2.csv")
 
 #Simple test
 
-features <-  train_users[ ,-c(1,16)]
+X <-  train_users[ ,-c(1,16)]
+X$gender <- ifelse(X$gender == 'MALE', 1, ifelse(X$gender == "FEMALE", 0, NA))
 target <- train_users[, c(16)]
 
-countriesList <- c('US', 'FR', 'CA', 'GB', 'ES', 'IT', 'PT', 'NL','DE', 'AU', 'NDF')
+
+countriesList <- c('US', 'FR', 'CA', 'GB', 'ES', 'IT', 'PT', 'NL','DE', 'AU', 'NDF', 'other')
 
 for (i in 1:(countriesList)){
-  currenctCountry = countriesList[i]
-  for (j in 1:)
+  y = ifelse(target == countriesList[i], 1, 0)
+  testSet =  data.frame(y, X)
+  fit <- rpart(y ~ X, method="class")
+  
+  
 }
 
 #Transform sessions
